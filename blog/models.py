9bc834d82cf2716b74dc503d887df0cf039dce7a
Adao Oliveira <adaum79@gmail.com>
2020-08-05 17:10:01 -0300

    Aula #04 finalizada

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.CharField(max_length=40),
+    title = models.CharField(max_length=100),
+    content = models.CharField(max_length=255),
+    date_posted = models.DateField(auto_now_add=True)
+
+    objetos = models.Manager()
