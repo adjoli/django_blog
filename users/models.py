9656d2642477b69760c2e583efdbce34fe0a7527
Adao Oliveira <adaum79@gmail.com>
2020-08-07 11:38:43 -0300

    Aula #09 finalizada (apresentando erro no local da pasta de 'default.jpg')

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,3 +9,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        # super(Profile, self).save(*args, **kwargs)
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 250 or img.width > 250:
+            output_size = (250, 250)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
